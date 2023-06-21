adf28383da226fe3b48ea97b0c8085ff2d517076
Muhammad Bilal <kkmuhammadbilal2@gmail.com>
2023-06-21 17:49:39 +0500

    ChartJs Completed

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {pie} from 'react-chartjs-2'
+import {Pie} from 'react-chartjs-2'
 // import { Chart } from 'chart.js/auto'
 const PieChart = ({data}) => {
   return (
     <div>
         <h1>Line CHART</h1>
-        <pie data={data}></pie>
+        <Pie data={data}></Pie>
     </div>
   )
 }
